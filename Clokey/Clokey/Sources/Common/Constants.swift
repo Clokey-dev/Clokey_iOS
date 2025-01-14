da941d8295281b8de7394282123e42e82b95b885
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-01-14 23:43:34 +0900

    feat: 프로젝트 추가 세팅
    - CustomButton 추가
    - CustomButtonTextField 추가
    - CustomNacigationBar 추가
    - CustomTextField 추가
    - Colorchip 추가
    - 커스텀 예시 뷰 추가
    
     #4

---
+++ Clokey/Clokey/Sources/Common/Constants.swift
@@ -0,0 +1,34 @@
+//
+//  Constants.swift
+//  Clokey
+//
+//  Created by 황상환 on 1/14/25.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct Colors {
+        // Main Colors
+        static let mainBrown50 = UIColor(named: "mainBrown50")
+        static let mainBrown200 = UIColor(named: "mainBrown200")
+        static let mainBrown400 = UIColor(named: "mainBrown400")
+        static let mainBrown600 = UIColor(named: "mainBrown600")
+        static let mainBrown800 = UIColor(named: "mainBrown800")
+        
+        // Point Colors
+        static let pointOrange200 = UIColor(named: "pointOrange200")
+        static let pointOrange400 = UIColor(named: "pointOrange400")
+        static let pointOrange600 = UIColor(named: "pointOrange600")
+        static let pointOrange800 = UIColor(named: "pointOrange800")
+        
+        // Text Colors
+        static let textGray0 = UIColor(named: "textGray0")
+        static let textGray200 = UIColor(named: "textGray200")
+        static let textGray400 = UIColor(named: "textGray400")
+        static let textGray600 = UIColor(named: "textGray600")
+        static let textGray800 = UIColor(named: "textGray800")
+    }
+}
+
---
+++ Clokey/Clokey/Sources/Feature/ViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by 황상환 on 1/13/25.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    let navBarManager = NavigationBarManager()
+
+    // 닉네임 필드 추가
+    let nicknameField = CustomTextField(title: "닉네임", placeholder: "2글자 이상", isRequired: true)
+
+    // 아이디 필드 추가
+    let IDField = CustomTextField(title: "아이디", placeholder: "ex) cky11", isRequired: false)
+
+    // 아이디 + 버튼
+    let idField = CustomButtonTextField(title: "아이디", placeholder: "ex) cky11", isRequired: true)
+    
+    // " " + 버튼
+    let idField2 = CustomButtonTextField(title: "", placeholder: "예) 회색 후드티, 검정 슬랙스 등", isRequired: false)
+    
+    // 비활성화 버튼 - false
+    let firstButton = CustomButton(title: "가입 완료", isEnabled: false)
+    
+    // 활성화 버튼 - true
+    let secondButton = CustomButton(title: "가입 완료", isEnabled: true)
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        // 네비게이션 뒤로가기
+        navBarManager.addBackButton(
+            to: navigationItem,
+            target: self,
+            action: #selector(didTapBackButton)
+        )
+
+        // 네비게이션 타이틀
+        navBarManager.setTitle(
+            to: navigationItem,
+            title: "커스텀 네비게이션 바",
+            font: .systemFont(ofSize: 18, weight: .semibold), textColor: .black
+        )
+
+        // 닉네임 필드 추가
+        view.addSubview(nicknameField)
+        nicknameField.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalToSuperview().offset(100)
+            $0.height.greaterThanOrEqualTo(60) // 적절한 높이 설정
+        }
+        
+        // 아이디 필드 추가
+        view.addSubview(IDField)
+        IDField.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(nicknameField.snp.bottom).offset(40)
+            $0.height.greaterThanOrEqualTo(60) // 적절한 높이 설정
+        }
+        
+        // 아이디 + 버튼
+        view.addSubview(idField)
+        idField.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(IDField.snp.bottom).offset(40)
+            make.height.equalTo(60)
+        }
+        
+        // " " + 버튼
+        view.addSubview(idField2)
+        idField2.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(idField.snp.bottom).offset(40)
+            make.height.equalTo(60)
+        }
+        
+        // " " + 버튼 + 활성화
+        idField2.setButtonEnabled(true)
+        idField2.setButtonText("중복확인")
+
+        // 비활성화 버튼
+        view.addSubview(firstButton)
+        firstButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(idField2.snp.bottom).offset(40)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        // 활성화 버튼
+        view.addSubview(secondButton)
+        secondButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(firstButton.snp.bottom).offset(40)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        // 버튼 텍스트 변경
+        secondButton.setTitle("활성화됨", for: .normal)
+        
+        
+        
+    }
+
+    @objc private func didTapBackButton() {
+        print("뒤로 버튼 클릭")
+        // NavigationController pop 동작
+        navigationController?.popViewController(animated: true)
+    }
+}
