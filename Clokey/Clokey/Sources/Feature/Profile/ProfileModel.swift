1615cee9748a43d4dbe0df60bed1e9d01c533293
geumjoon <152842614+Funital@users.noreply.github.com>
2025-01-27 17:47:05 +0900

    feat: 내 프로필 화면 UI 구현
    - 배경사진 및 프로필 사진 배치
    
    - 옷장 및 기록 파트 UI 배치
    
    #12

---
+++ Clokey/Clokey/Sources/Feature/Profile/ProfileModel.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileModel.swift
+//  Clokey
+//
+//  Created by 한금준 on 1/27/25.
+//
+
+import UIKit
+
+// 모델 정의
+struct ProfileModel {
+    let profileImageURLs: [String]
+}
+
+// 더미 데이터 관련 기능을 분리한 extension
+extension ProfileModel {
+    static func dummy() -> ProfileModel {
+        return ProfileModel(
+            profileImageURLs: [
+                "https://img.danawa.com/prod_img/500000/436/224/img/17224436_1.jpg?_v=20220610092752",
+                "https://www.ocokorea.com//upload/images/product/111/111888/Product_1670035608378.jpg",
+                "https://item.elandrs.com/r/image/item/2023-10-13/fbb4c2ed-930a-4cb8-97e0-d4f287a1c971.jpg?w=750&h=&q=100"
+            ]
+        )
+    }
+}
