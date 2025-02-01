32ec5686bb41902ce5808352924eaf86db1a4dcd
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-02-02 05:06:08 +0900

    fix: SceneDelegate 수정
    - SceneDelegate 수정

--- Clokey/Clokey/Application/SceneDelegate.swift
+++ Clokey/Clokey/Application/SceneDelegate.swift
@@ -28,7 +28,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         
         // 로그인 화면을 초기 화면으로 설정
-        let loginViewController = CalendarDetailViewController()
+        let loginViewController = LoginViewController(coordinator: self)
         window?.rootViewController = loginViewController
        
 //        let loginViewController = CalendarDetailViewController()
