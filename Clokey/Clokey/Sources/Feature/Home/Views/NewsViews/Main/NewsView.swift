ccb771182247843d0127cc0be2b9e4cd6a576f4b
geumjoon <152842614+Funital@users.noreply.github.com>
2025-03-07 13:07:56 +0900

    fix: 뷰 디폴트값 없애기
    - 팝업창, 홈 뷰 디폴트 라벨값 지우기
    
    #86

--- Clokey/Clokey/Sources/Feature/Home/Views/NewsViews/Main/NewsView.swift
+++ Clokey/Clokey/Sources/Feature/Home/Views/NewsViews/Main/NewsView.swift
@@ -72,13 +72,13 @@ class NewsView: UIView {
     }
     
     let usernameLabel: UILabel = UILabel().then {
-        $0.text = "Sena_05"
+        $0.text = ""
         $0.textColor = .black
         $0.font = UIFont.ptdMediumFont(ofSize: 14)
     }
     
     let dateLabel: UILabel = UILabel().then {
-        $0.text = "2024.11.27"
+        $0.text = ""
         $0.textColor = .black
         $0.font = UIFont.ptdMediumFont(ofSize: 12)
     }
@@ -144,7 +144,7 @@ class NewsView: UIView {
     }
     
     let followingCalendarUpdateSubTitle: UILabel = UILabel().then {
-        $0.text = "2024.11.27" // 부제목 텍스트
+        $0.text = "" // 부제목 텍스트
         $0.textColor = .black // 텍스트 색상
         $0.font = UIFont.ptdRegularFont(ofSize: 14) // 작은 폰트 크기
         $0.numberOfLines = 0 // 여러 줄 허용
@@ -169,7 +169,7 @@ class NewsView: UIView {
     let followingCalendarProfileName1 = UILabel().then{
         $0.font = UIFont.ptdMediumFont(ofSize: 14)
         $0.textColor = .black
-        $0.text = "티라미수케이크"
+        $0.text = ""
     }
     
     let followingCalendarUpdateImageView2: UIImageView = UIImageView().then {
@@ -187,7 +187,7 @@ class NewsView: UIView {
     let followingCalendarProfileName2 = UILabel().then{
         $0.font = UIFont.ptdMediumFont(ofSize: 14)
         $0.textColor = .black
-        $0.text = "티라미수케이크"
+        $0.text = ""
     }
     
     let followingCalendarBottomButton: UIStackView = UIStackView().then {
@@ -236,7 +236,7 @@ class NewsView: UIView {
     let hotAccountProfileName1 = UILabel().then{
         $0.font = UIFont.ptdMediumFont(ofSize: 14)
         $0.textColor = UIColor(red: 52/255, green: 52/255, blue: 52/255, alpha: 1.0)
-        $0.text = "티라미수케이크"
+        $0.text = ""
     }
     
     let hotAccountImageView2: UIImageView = UIImageView().then {
@@ -254,7 +254,7 @@ class NewsView: UIView {
     let hotAccountProfileName2 = UILabel().then{
         $0.font = UIFont.ptdMediumFont(ofSize: 14)
         $0.textColor = UIColor(red: 38/255, green: 38/255, blue: 38/255, alpha: 1.0)
-        $0.text = "티라미수케이크"
+        $0.text = ""
     }
     
     let hotAccountImageView3: UIImageView = UIImageView().then {
@@ -272,7 +272,7 @@ class NewsView: UIView {
     let hotAccountProfileName3 = UILabel().then{
         $0.font = UIFont.ptdMediumFont(ofSize: 14)
         $0.textColor = UIColor(red: 38/255, green: 38/255, blue: 38/255, alpha: 1.0)
-        $0.text = "티라미수케이크"
+        $0.text = ""
     }
     
     let hotAccountImageView4: UIImageView = UIImageView().then {
@@ -290,7 +290,7 @@ class NewsView: UIView {
     let hotAccountProfileName4 = UILabel().then{
         $0.font = UIFont.ptdMediumFont(ofSize: 14)
         $0.textColor = UIColor(red: 38/255, green: 38/255, blue: 38/255, alpha: 1.0)
-        $0.text = "티라미수케이크"
+        $0.text = ""
     }
     
     // MARK: - Initializer
