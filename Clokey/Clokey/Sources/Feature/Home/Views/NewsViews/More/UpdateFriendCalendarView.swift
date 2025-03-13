f62285ee80fa7f03ab44f6dc44c702c1fdd33897
geumjoon <152842614+Funital@users.noreply.github.com>
2025-03-13 19:21:31 +0900

    fix: 소식 더보기 뷰 네비게이션바 수정
    - 팔로우 옷장 및 캘린더 더보기 뷰의 네비게이션바를 공통 네브바로 수정
    
    #86

--- Clokey/Clokey/Sources/Feature/Home/Views/NewsViews/More/UpdateFriendCalendarView.swift
+++ Clokey/Clokey/Sources/Feature/Home/Views/NewsViews/More/UpdateFriendCalendarView.swift
@@ -22,36 +22,8 @@ class UpdateFriendCalendarView: UIView {
         $0.backgroundColor = .white
     }
     
-    let backButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "chevron.left"), for: .normal)
-        $0.tintColor = .black
-    }
-    
-    let title: UILabel = UILabel().then {
-        let fullText = "친구의 캘린더 업데이트 소식"
-        let targetText = "캘린더"
-        let attributedString = NSMutableAttributedString(string: fullText)
-        
-        // 전체 텍스트 스타일
-        attributedString.addAttributes([
-            .font: UIFont.ptdMediumFont(ofSize: 20),
-            .foregroundColor: UIColor.black
-        ], range: NSRange(location: 0, length: fullText.count))
-        
-        // "옷장"에 다른 스타일 적용
-        if let targetRange = fullText.range(of: targetText) {
-            let nsRange = NSRange(targetRange, in: fullText)
-            attributedString.addAttributes([
-                .font: UIFont.ptdSemiBoldFont(ofSize: 20), // 예시로 굵게 처리
-                .foregroundColor: UIColor.black // 색상을 변경하려면 여기 설정
-            ], range: nsRange)
-        }
-        
-        $0.attributedText = attributedString
-    }
-    
     let subTitle: UILabel = UILabel().then {
-        $0.text = ""
+        $0.text = "dd"
         $0.font = UIFont.ptdRegularFont(ofSize: 14)
         $0.textColor = .black
     }
@@ -85,15 +57,14 @@ class UpdateFriendCalendarView: UIView {
         
         addSubview(scrollView)
         scrollView.addSubview(contentView)
-        contentView.addSubview(backButton)
-        contentView.addSubview(title)
         contentView.addSubview(subTitle)
         contentView.addSubview(updateFriendCalendarCollectionView)
     }
     
     private func setupConstraints() {
         scrollView.snp.makeConstraints { make in
-            make.edges.equalToSuperview() // 화면 전체에 ScrollView
+            make.top.equalTo(safeAreaLayoutGuide)
+            make.leading.trailing.bottom.equalToSuperview()
         }
         
         // ContentView 제약 설정
@@ -103,20 +74,10 @@ class UpdateFriendCalendarView: UIView {
             make.bottom.equalTo(updateFriendCalendarCollectionView.snp.bottom).offset(20)
             
         }
-        
-        backButton.snp.makeConstraints { make in
-            make.top.equalTo(contentView.safeAreaLayoutGuide).offset(20)
-            make.leading.equalToSuperview().offset(20)
-            make.size.equalTo(CGSize(width: 10, height: 20))
-        }
-        
-        title.snp.makeConstraints { make in
-            make.centerY.equalTo(backButton)
-            make.leading.equalTo(backButton.snp.trailing).offset(20)
-        }
+
         
         subTitle.snp.makeConstraints { make in
-            make.top.equalTo(title.snp.bottom).offset(15)
+            make.top.equalToSuperview().offset(10)
             make.leading.equalToSuperview().offset(20)
             make.width.equalTo(233)
             make.height.equalTo(16)
@@ -125,7 +86,7 @@ class UpdateFriendCalendarView: UIView {
         updateFriendCalendarCollectionView.snp.makeConstraints{ make in
             make.top.equalTo(subTitle.snp.bottom).offset(15)
             make.leading.trailing.equalToSuperview().inset(20)
-            make.height.equalTo(1) // 초기 높이 (1로 설정하여 콘텐츠 크기 업데이트 유도)
+            make.height.equalTo(300) // 초기 높이 (1로 설정하여 콘텐츠 크기 업데이트 유도)
             
         }
     }
